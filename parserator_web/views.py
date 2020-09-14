1be9db581339ab2f774c7187e7d3d64fa044310b
Noah Marcus <Walcott@Noahs-MBP.home>
2020-09-13 22:18:01 -0400

    WIP: completed steps one - three of coding challenge

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -13,12 +13,33 @@ class Home(TemplateView):
 class AddressParse(APIView):
     renderer_classes = [JSONRenderer]
 
+    # parse requested address string and return components to frontend 
+    #       input: request {input_string; String} from frontend
+    #       output: input_string [String], 
+    #               address_components [Dictionary {address_part: tag}], 
+    #               address_type [String]
     def get(self, request):
-        # TODO: Flesh out this method to parse an address string using the
-        # parse() method and return the parsed components to the frontend.
-        return Response({})
+        # get user's input_string from request data
+        input_string = request.query_params['input_string']
 
+        # pass request address to parse method
+        address_components, address_type = self.parse(input_string)
+        
+        # generate response JSON
+        response_data = { 'input_string': input_string, \
+                          'address_components': address_components, \
+                          'address_type': address_type }
+        
+        return Response(response_data)
+
+    # return the parsed components of a given address using usaddress
+    #       input: address [String]
+    #       output: address_components [Dictionary {address_part: tag}],
+    #               address_type [String]
     def parse(self, address):
-        # TODO: Implement this method to return the parsed components of a
-        # given address using usaddress: https://github.com/datamade/usaddress
+        # using usaddress tag() method to parse address string
+        #       input: address string
+        #       output: OrderedDict(Address Components), String (Address Type)
+        address_components, address_type = usaddress.tag(address)
+
         return address_components, address_type
