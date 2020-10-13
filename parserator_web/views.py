1c9bb8db8a9eb335017bfc6fe67e4230a3160deb
Noah Marcus <Walcott@Noahs-MBP.home>
2020-10-13 18:57:49 -0400

    + turned function description comments into docstrings

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -13,12 +13,18 @@ class Home(TemplateView):
 class AddressParse(APIView):
     renderer_classes = [JSONRenderer]
 
-    # parse requested address string and return components to frontend 
-    #       input: request {input_string; String} from frontend
-    #       output: input_string [String], 
-    #               address_components [Dictionary {address_part: tag}], 
-    #               address_type [String]
     def get(self, request):
+        """Parse requested address string and return components to frontend 
+
+        Arguments
+            request - request from frontend {input_string; String} 
+
+        Output
+            input_string [String]
+            address_components [Dictionary {address_part: tag}]
+            address_type [String]
+        """
+
         # get user's input_string from request data
         input_string = request.query_params['input_string']
 
@@ -41,11 +47,19 @@ class AddressParse(APIView):
         # return Response with data and status code
         return Response(response_data)
 
-    # return the parsed components of a given address using usaddress
-    #       input: address [String]
-    #       output: address_components [Dictionary {address_part: tag}],
-    #               address_type [String]
+
     def parse(self, address):
+        """Return the parsed components of a given address using usaddress
+
+        Arguments
+            address [String]
+        
+        Output
+            address_components [Dictionary {address_part: tag}]
+            address_type [String]
+        """
+
+
         # using usaddress tag() method to parse address string
         #       input: address string
         #       output: OrderedDict(Address Components), String (Address Type)
